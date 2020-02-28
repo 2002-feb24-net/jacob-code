0e73413176b7393d47c53589cb5eef214c35234a
Jacob Koch <jacob.koch.utd@gmail.com>
2020-02-28 09:03:18 -0600

    Added Main and AcronymSolver Method

--- practice-code/Acronym/Program.cs
+++ practice-code/Acronym/Program.cs
@@ -6,7 +6,20 @@ namespace Acronym
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter multi word term: ");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            AcronymSolver(words);
+        }
+        static void AcronymSolver(string[] words)
+        {
+            string temp = "";
+            Console.Write("Acronym: ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                temp = words[i];
+                Console.Write(char.ToUpper(temp[0]));
+            }
         }
     }
 }
