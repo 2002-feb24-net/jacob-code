9cddd7385d6438303aee085858dfe993cb9d7df3
Jacob Koch <jacob.koch.utd@gmail.com>
2020-03-03 11:06:14 -0600

    More reorganization

---
+++ WeekB/DemoObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
