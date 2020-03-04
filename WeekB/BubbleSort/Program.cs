24c36273bbfab40f4571e047095603469a79554a
Jacob Koch <jacob.koch.utd@gmail.com>
2020-03-04 16:27:36 -0600

    added bubblesort

---
+++ WeekB/BubbleSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {3,2,1,4,6};
+            bool sorted = false;
+            bool noSwaps = true;
+            int tempSwap = 0;
+            while(sorted==false)
+            {
+                noSwaps = true;
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if(a[i]<a[i-1])
+                    {
+                        tempSwap = a[i];
+                        noSwaps = false;
+                        a[i] = a[i-1];
+                        a[i-1] = tempSwap;
+                    }
+                }
+                if(noSwaps==true)
+                {
+                    sorted=true;
+                }
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(" " + a[i]);
+            }
+        }
+    }
+}
