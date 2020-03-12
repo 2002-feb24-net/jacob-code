617eb09afa45b475c5b7c62e08d8415061c563d7
Jacob Koch <jacob.koch.utd@gmail.com>
2020-03-12 14:23:43 -0500

    added generic example and day 14 notes

---
+++ Week1/Generics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayLists();
+
+            // whenever we use a type with <> at the end
+            // that is a generic type
+            // and the specific thing you p;ut in the <> is called the type parameter.
+            var list = new List<int>();
+            var list2 = new List<string>();
+            var sortedList = new SortedSequence<string>();
+            sortedList.Add("asdf");
+            sortedList.Add("b");
+            sortedList.Add("fsff");
+            sortedList.Add("af");
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write(sortedList[i] + " ");
+            }
+        }
+
+        static void ArrayLists()
+        {
+            var arraylist = new ArrayList();
+            arraylist.Add(1);
+            arraylist.Add(2);
+            arraylist.Add(3);
+            //arraylist.Add(null);
+            arraylist.Add(new ArrayList());
+            arraylist.Add("asdg");
+
+            //when used with the non generic IEnumerable interface
+            //it will attempt to cast to whatever type you put here
+            //       v
+            foreach(int item in arraylist)
+            {
+                //int num2 = (int)item;
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ Week1/Generics/SortedSequence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    // behaves like a List<T> but keep itself sorted
+
+    // you declase a class's type parameter with <(name)> right after the class name
+    //the name can be any type
+    //after the where come type constraints
+    // e.g. class, struct, any specific type
+    class SortedSequence<T> : IEnumerable<T> where T : class
+    {
+        List<T> _list = new List<T>();
+        
+        //lets say for no reason
+        //this class always starts out with one element in it
+        //which will be the default value of that type.
+        public SortedSequence()
+        {
+            Add(default);
+        }
+        public void Add(T item)
+        {
+            _list.Add(item);
+            _list.Sort();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)_list).GetEnumerator();
+        }
+
+
+        // implement indexer operator
+        public T this[int i]
+        {
+            get => _list[i];
+            set => _list[i] = value;
+            // (using expression body syntax so its easier to write)
+        }
+    }
+}
