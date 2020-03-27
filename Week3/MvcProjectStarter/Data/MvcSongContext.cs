e246fa7eabdb31852dc5996ef12d291aa44314b9
Jacob Koch <jacob.koch.utd@gmail.com>
2020-03-27 09:20:10 -0500

    MVC Project Starter Day one

---
+++ Week3/MvcProjectStarter/Data/MvcSongContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcProjectStarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcProjectStarter.Data
+{
+    public class MvcSongContext : DbContext
+    {
+        public MvcSongContext(DbContextOptions<MvcSongContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+    }
+}
---
+++ Week3/MvcProjectStarter/Models/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcProjectStarter.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcProjectStarter.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new MvcSongContext(serviceProvider.GetRequiredService<DbContextOptions<MvcSongContext>>()))
+            {
+                if(context.Songs.Any())
+                {
+                    return;//songs in the DB. We are good to go
+                }
+
+                context.Songs.AddRange(
+                    new Song
+                    { title = "Don't stop Believin'",
+                        genre = "Rock",
+                        artist = "Journey",
+                        album = "Unknown",
+                        ReleaseDate = DateTime.Parse("1981-3-13")
+                    },
+                    new Song
+                    {
+                        title = "Thriller",
+                        genre = "Rock",
+                        artist = "Michael J",
+                        album = "Thriller",
+                        ReleaseDate = DateTime.Parse("1981-4-14")
+                    },
+                    new Song
+                    {
+                        title = "Eye of the Tiger",
+                        genre = "Rock",
+                        artist = "Survivor",
+                        album = "Unknown",
+                        ReleaseDate = DateTime.Parse("1985-3-13")
+                    }
+                    );
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
