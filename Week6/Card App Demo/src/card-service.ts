a14b7a363196295764ec2877fe3ce470855fa732
Jacob Koch <jacob.koch.utd@gmail.com>
2020-04-16 10:03:25 -0500

    setting up tour of heroes

--- Week6/Card App Demo/src/card-service.ts
+++ Week6/Card App Demo/src/card-service.ts
@@ -1,9 +1,13 @@
 import Card from "./card";
 export default class CardService {
     drawCard(): Promise<void> {
+        .then(){
+
+        };
         throw new Error("Method not implemented.");
     }
     newDeck(): Promise<void> {
+        return '';
         throw new Error("Method not implemented.");
     }
 }
